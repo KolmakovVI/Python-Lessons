247f985bcd9a76e36b9f75eca3a25711288eab27
Kolmakov VI <kolmakowladimir@gmail.com>
2022-02-22 22:10:37 +0300

    change for circle to while circle and change var line to var numb_line

--- Python_for_jun/SubTranslator.py
+++ Python_for_jun/SubTranslator.py
@@ -11,18 +11,18 @@ inp_file_path = r"C:\Users\kolma\Desktop\Courses\Иллюстрация\InHyuk L
 out_file_path = r"C:\Users\kolma\Desktop\Courses\Иллюстрация\InHyuk Lee Illustration\0 Hello, I'm InHyuk Lee\subtitles\Этот файл переделать!!!.txt"
 with open(inp_file_path, 'r') as inf, open(out_file_path, 'w') as ouf:  # create a construction for open files
     inf_all = inf.readlines()  # set all lines to list
-    for line in len(inf_all):  # нужно найти способ, чтобы можно было прыгнуть сразу через одну строку в цикле, если я уже две строки соединил и перевел.
+    while numb_line <= len(inf_all):  # нужно найти способ, чтобы можно было прыгнуть сразу через одну строку в цикле, если я уже две строки соединил и перевел.
         print(str(numb_line))
         if numb_line < 2: # skip two first lines for translator
-            ouf.write(line) # just write two first lines to new file
+            ouf.write(numb_line) # just write two first lines to new file
             numb_line += 1
         else:
             # if first element of line is word OR bracket -- we must translate this line
-            if line[0].isalpha() or line[0] == '(':
+            if numb_line[0].isalpha() or numb_line[0] == '(':
                 time.sleep(0.3)  # set sleep time for request limit of goog translator
-                new_line = translator.translate(line, src='english',
+                new_line = translator.translate(numb_line, src='english',
                                                 dest='russian')  # create a new line with the translated text
-                ouf.write(line)  # write original english line to new file
+                ouf.write(numb_line)  # write original english line to new file
                 ouf.write(new_line.text + '\n')  # write translated line to new file
                 '''
                 output:
@@ -33,5 +33,5 @@ with open(inp_file_path, 'r') as inf, open(out_file_path, 'w') as ouf:  # create
                 текст
                 '''
             else:  # if line don't have word or bracket
-                ouf.write(line)  # just write this line to new file
+                ouf.write(numb_line)  # just write this line to new file
             numb_line += 1  # increase number of line
