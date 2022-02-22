17c42182f2e047ed4019bc1466aeb3bec245a748
Kolmakov VI <kolmakowladimir@gmail.com>
2022-02-22 21:50:14 +0300

    create the comments

--- Python_for_jun/SubTranslator.py
+++ Python_for_jun/SubTranslator.py
@@ -1,27 +1,37 @@
-import time
+import time  # import timeLib for sleep func
 
-import googletrans
+import googletrans  # import for translation
 
-translator = googletrans.Translator()
-numb_line = 0 # another little 
+translator = googletrans.Translator()  # set VAR for func
+numb_line = 0  # set number of line in file
 
+# set a path for input file with subtitles
 inp_file_path = r"C:\Users\kolma\Desktop\Courses\Иллюстрация\InHyuk Lee Illustration\0 Hello, I'm InHyuk Lee\subtitles\en-InHyuk Lee-Intermediate-0-1-edit.vtt"
+# set a PATh to output file
 out_file_path = r"C:\Users\kolma\Desktop\Courses\Иллюстрация\InHyuk Lee Illustration\0 Hello, I'm InHyuk Lee\subtitles\Этот файл переделать!!!.txt"
-with open(inp_file_path, 'r') as inf, open(out_file_path, 'w') as ouf:
-    inf_all = inf.readlines()
-    for line in inf_all: # нужно найти способ, чтобы можно было прыгнуть сразу через одну строку в цикле, если я уже две строки соединил и перевел.
-
+with open(inp_file_path, 'r') as inf, open(out_file_path, 'w') as ouf:  # create a cotstruction for open files
+    inf_all = inf.readlines()  # set all lines to list
+    for line in inf_all:  # нужно найти способ, чтобы можно было прыгнуть сразу через одну строку в цикле, если я уже две строки соединил и перевел.
         print(str(numb_line))
         if numb_line < 2:
             ouf.write(line)
             numb_line += 1
         else:
-
+            # if first element of line is word OR bracket -- we must translate this line
             if line[0].isalpha() or line[0] == '(':
-                time.sleep(0.3)
-                new_line = translator.translate(line, src='english', dest='russian')
-                ouf.write(line)
-                ouf.write(new_line.text + '\n')
-            else:
-                ouf.write(line)
-            numb_line += 1
+                time.sleep(0.3)  # set sleep time for request limit of goog translator
+                new_line = translator.translate(line, src='english',
+                                                dest='russian')  # create a new line with the translated text
+                ouf.write(line)  # write original english line to new file
+                ouf.write(new_line.text + '\n')  # write translated line to new file
+                '''
+                output:
+                ---------------------
+                original
+                оригинальный
+                text
+                текст
+                '''
+            else:  # if line don't have word or bracket
+                ouf.write(line)  # just write this line to new file
+            numb_line += 1  # increase number of line
