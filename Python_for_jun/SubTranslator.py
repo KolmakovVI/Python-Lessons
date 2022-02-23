cad9ea48069defcee463e1e7625f15cac3b7abff
Kolmakov VI <kolmakowladimir@gmail.com>
2022-02-23 17:08:34 +0300

    add the condition which break a cycly when a file end

--- Python_for_jun/SubTranslator.py
+++ Python_for_jun/SubTranslator.py
@@ -14,19 +14,22 @@ with open(inp_file_path, 'r') as inf, open(out_file_path, 'w',
     inf_all = inf.readlines()  # set all lines to list
     # execute cycle while the number of line don't equal last number of line on input file
     while numb_line <= len(inf_all):
+
         print(str(numb_line))
+
         if numb_line < 2:  # skip two first lines for translator
             ouf.write(inf_all[numb_line])  # just write two first lines to new file
             numb_line += 1  # increase number of line
-        else:
-            # if first element of line is word OR bracket -- we must translate this line
+
+        elif numb_line <= len(inf_all):
+
             if inf_all[numb_line][0].isalpha() or inf_all[numb_line][0] == '(':
                 lines_to_trans.append(inf_all[numb_line])  # append new line to list for translator
                 ouf.write(inf_all[numb_line])  # just write this line to new file
 
             else:  # if line don't have word or bracket
                 for line in lines_to_trans:
-                    time.sleep(0.1)  # set sleep time for request limit of goog translator
+                    time.sleep(0.2)  # set sleep time for request limit of goog translator
                     new_line = translator.translate(line, src='english',
                                                     dest='russian')  # create a new line with the translated text
                     ouf.write(new_line.text + '\n')  # write original english line to new file
@@ -42,6 +45,10 @@ with open(inp_file_path, 'r') as inf, open(out_file_path, 'w',
 
                 lines_to_trans.clear()  # set empty list for searching next fragments subtitles
                 ouf.write(inf_all[numb_line])  # just write this line to new file
+            # if first element of line is word OR bracket -- we must translate this line
 
             numb_line += 1  # increase number of line
+
+        else:
+            break
 
--- Python_for_jun/test.py
+++ Python_for_jun/test.py
@@ -18,10 +18,20 @@
 # elif c == "div":
 #     print(a // b)
 
+
+import translators as ts
+import time
 # set a path for input file with subtitles
-inp_file_path = r"C:\Users\kolma\Desktop\Courses\Иллюстрация\InHyuk Lee Illustration\0 Hello, I'm InHyuk Lee\subtitles\Change_this_FILE.vtt"
+inp_file_path = r"C:\Users\kolma\Desktop\Courses\Иллюстрация\InHyuk Lee Illustration\0 Hello, I'm InHyuk Lee\subtitles\en-InHyuk Lee-Intermediate-0-1-edit.vtt"
 # set a PATh to output file
-out_file_path = r"C:\Users\kolma\Desktop\Courses\Иллюстрация\InHyuk Lee Illustration\0 Hello, I'm InHyuk Lee\subtitles\test.vtt"
+out_file_path = r"C:\Users\kolma\Desktop\Courses\Иллюстрация\InHyuk Lee Illustration\0 Hello, I'm InHyuk Lee\subtitles\testnewtr.vtt"
+tic = 0
 with open(inp_file_path, 'r') as inf, open(out_file_path, 'w', encoding="utf-8") as ouf:  # create a construction for open files
     for line in inf:
-        ouf.write(line)
+        if line[0].isalpha() or line[0] == '(':
+            # ouf.write(line)
+            transl_line = ts.google(line, from_language='en', to_language='ru')
+            # ouf.write(t)
+            print(transl_line)
+            time.sleep(1)
+            # tic+=1
