e170615d2c7dd318065f7be075ab4c37376e19a4
Kolmakov VI <kolmakowladimir@gmail.com>
2022-02-23 14:34:39 +0300

    add converting output file to UTF-8. This full work version, but translator is bad

--- Python_for_jun/SubTranslator.py
+++ Python_for_jun/SubTranslator.py
@@ -8,15 +8,16 @@ lines_to_trans = []  # create a list for accumulating lines to translate
 # set a path for input file with subtitles
 inp_file_path = r"C:\Users\kolma\Desktop\Courses\Иллюстрация\InHyuk Lee Illustration\0 Hello, I'm InHyuk Lee\subtitles\en-InHyuk Lee-Intermediate-0-1-edit.vtt"
 # set a PATh to output file
-out_file_path = r"C:\Users\kolma\Desktop\Courses\Иллюстрация\InHyuk Lee Illustration\0 Hello, I'm InHyuk Lee\subtitles\Change_this_FILE.vtt"
-with open(inp_file_path, 'r') as inf, open(out_file_path, 'w') as ouf:  # create a construction for open files
+out_file_path = r"C:\Users\kolma\Desktop\Courses\Иллюстрация\InHyuk Lee Illustration\0 Hello, I'm InHyuk Lee\subtitles\RU-InHyuk Lee-Intermediate-0-1-edit.vtt"
+with open(inp_file_path, 'r') as inf, open(out_file_path, 'w',
+                                           encoding="utf-8") as ouf:  # create a construction for open files
     inf_all = inf.readlines()  # set all lines to list
     # execute cycle while the number of line don't equal last number of line on input file
     while numb_line <= len(inf_all):
         print(str(numb_line))
         if numb_line < 2:  # skip two first lines for translator
             ouf.write(inf_all[numb_line])  # just write two first lines to new file
-            numb_line += 1 # increase number of line
+            numb_line += 1  # increase number of line
         else:
             # if first element of line is word OR bracket -- we must translate this line
             if inf_all[numb_line][0].isalpha() or inf_all[numb_line][0] == '(':
@@ -25,7 +26,7 @@ with open(inp_file_path, 'r') as inf, open(out_file_path, 'w') as ouf:  # create
 
             else:  # if line don't have word or bracket
                 for line in lines_to_trans:
-                    time.sleep(0.2)  # set sleep time for request limit of goog translator
+                    time.sleep(0.1)  # set sleep time for request limit of goog translator
                     new_line = translator.translate(line, src='english',
                                                     dest='russian')  # create a new line with the translated text
                     ouf.write(new_line.text + '\n')  # write original english line to new file
@@ -37,15 +38,10 @@ with open(inp_file_path, 'r') as inf, open(out_file_path, 'w') as ouf:  # create
                 #     new_line = translator.translate(full_string, src='english',
                 #                                         dest='russian')  # create a new line with the translated text
                 #     ouf.write(new_line.text + '\n')  # write original english line to new file
+                # ----------------- this feature don't increase quality of translated text
 
-                lines_to_trans = []  # set empty list for searching next fragments subtitles
-                '''
-                output:
-                ---------------------
-                original
-                оригинальный
-                text
-                текст
-                '''
+                lines_to_trans.clear()  # set empty list for searching next fragments subtitles
                 ouf.write(inf_all[numb_line])  # just write this line to new file
+
             numb_line += 1  # increase number of line
+
--- Python_for_jun/test.py
+++ Python_for_jun/test.py
@@ -17,8 +17,11 @@
 #     print(a % b)
 # elif c == "div":
 #     print(a // b)
-lines_to_trans = ["What are some contributing factors\n", "say hello\n"]
-#full_string = " ".join(lines_to_trans).replace('\n', '')
-full_string = " ".join(lines_to_trans)
-print(type(full_string))
-print(full_string)
+
+# set a path for input file with subtitles
+inp_file_path = r"C:\Users\kolma\Desktop\Courses\Иллюстрация\InHyuk Lee Illustration\0 Hello, I'm InHyuk Lee\subtitles\Change_this_FILE.vtt"
+# set a PATh to output file
+out_file_path = r"C:\Users\kolma\Desktop\Courses\Иллюстрация\InHyuk Lee Illustration\0 Hello, I'm InHyuk Lee\subtitles\test.vtt"
+with open(inp_file_path, 'r') as inf, open(out_file_path, 'w', encoding="utf-8") as ouf:  # create a construction for open files
+    for line in inf:
+        ouf.write(line)
